c14a3d4b6e592166042d07663c586be500ee5541
Funix565 <denkin.dmitriy@gmail.com>
2022-12-06 18:57:28 +0200

    Migrate from ViewPager to ViewPager2

--- app/src/main/java/ua/sd/messagehider/MainActivity.kt
+++ app/src/main/java/ua/sd/messagehider/MainActivity.kt
@@ -1,15 +1,15 @@
 package ua.sd.messagehider
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
-import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import ua.sd.messagehider.ui.main.SectionsPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import ua.sd.messagehider.databinding.ActivityMainBinding
+import ua.sd.messagehider.ui.main.SectionsPagerAdapter
+import ua.sd.messagehider.ui.main.TAB_TITLES
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,11 +21,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = binding.viewPager
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        val viewPager: ViewPager2 = binding.viewPager
         viewPager.adapter = sectionsPagerAdapter
+
+        // To disable swiping
+        viewPager.isUserInputEnabled = false
+
         val tabs: TabLayout = binding.tabs
-        tabs.setupWithViewPager(viewPager)
+
+        // TabLayout requires a TabLayoutMediator instance to integrate with ViewPager2
+        // The TabLayoutMediator object also handles the task
+        // of generating page titles for the TabLayout object.
+        // Previously: SectionsPagerAdapter.getPageTitle()
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = this.resources.getString(TAB_TITLES[position])
+        }.attach()
+
         val fab: FloatingActionButton = binding.fab
 
         fab.setOnClickListener { view ->
--- app/src/main/java/ua/sd/messagehider/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/ua/sd/messagehider/ui/main/SectionsPagerAdapter.kt
@@ -1,35 +1,28 @@
 package ua.sd.messagehider.ui.main
 
-import android.content.Context
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import ua.sd.messagehider.R
 
-private val TAB_TITLES = arrayOf(
+val TAB_TITLES = arrayOf(
     R.string.tab_text_1,
     R.string.tab_text_2
 )
 
 /**
- * A [FragmentPagerAdapter] that returns a fragment corresponding to
- * one of the sections/tabs/pages.
+ * A [FragmentStateAdapter] to page through a small, fixed number of fragments.
  */
-class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
-    FragmentPagerAdapter(fm) {
+class SectionsPagerAdapter(fa: FragmentActivity) :
+    FragmentStateAdapter(fa) {
 
-    override fun getItem(position: Int): Fragment {
-        // getItem is called to instantiate the fragment for the given page.
+    override fun createFragment(position: Int): Fragment {
+        // createFragment always supplies a new fragment instance each time the function is called.
         // Return a PlaceholderFragment (defined as a static inner class below).
         return PlaceholderFragment.newInstance(position + 1)
     }
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
-    }
-
-    override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
+    override fun getItemCount(): Int {
+        return TAB_TITLES.size
     }
 }
